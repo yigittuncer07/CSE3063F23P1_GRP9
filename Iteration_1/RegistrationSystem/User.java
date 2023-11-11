2c72dacf90b05cb98490cc4ae26f727a5aa6f579
yigittuncer <yigittuncer@marun.edu.tr>
2023-11-11 19:42:51 +0300

    Addded getter setter methods

--- Iteration_1/RegistrationSystem/User.java
+++ Iteration_1/RegistrationSystem/User.java
@@ -1,5 +1,5 @@
 abstract class User {
-    private String user;
+    private String firstName;
     private String secondName;
     private String lastName;
     private String birthDate;
@@ -8,5 +8,79 @@ abstract class User {
     private String email;
     private String username;
     private String password;
-    
+
+    // Getter methods
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setter methods
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
