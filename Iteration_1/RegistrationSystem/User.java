ed2a1575e2a40ec78059ca3641757520c26a9a6a
yigittuncer <yigittuncer@marun.edu.tr>
2023-11-11 17:11:07 +0300

    Added abstract user class

---
+++ Iteration_1/RegistrationSystem/User.java
@@ -0,0 +1,12 @@
+abstract class User {
+    private String user;
+    private String secondName;
+    private String lastName;
+    private String birthDate;
+    private String address;
+    private String ssn;
+    private String email;
+    private String username;
+    private String password;
+    
+}
