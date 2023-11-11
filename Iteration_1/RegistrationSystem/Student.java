905d17a51ea4510a19dd26a2f038b11bae5eccf9
yigittuncer <yigittuncer@marun.edu.tr>
2023-11-11 19:43:50 +0300

    Added getter setter methods

--- Iteration_1/RegistrationSystem/Student.java
+++ Iteration_1/RegistrationSystem/Student.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 class Student extends User{
     private Transcript transcript;
     private String studentId;
-    private Advisor Advisor;
+    private Advisor advisor;
     private ArrayList<Course> registeredCourses;
 
     public void printTranscriptInformation(Transcript transcript){
@@ -13,4 +13,38 @@ class Student extends User{
     public boolean registerToCourse(Advisor advisor, String stdID){
         return false;
     }
+
+        // Getter methods
+        public Transcript getTranscript() {
+            return transcript;
+        }
+    
+        public String getStudentId() {
+            return studentId;
+        }
+    
+        public Advisor getAdvisor() {
+            return advisor;
+        }
+    
+        public ArrayList<Course> getRegisteredCourses() {
+            return registeredCourses;
+        }
+    
+        // Setter methods
+        public void setTranscript(Transcript transcript) {
+            this.transcript = transcript;
+        }
+    
+        public void setStudentId(String studentId) {
+            this.studentId = studentId;
+        }
+    
+        public void setAdvisor(Advisor advisor) {
+            this.advisor = advisor;
+        }
+    
+        public void setRegisteredCourses(ArrayList<Course> registeredCourses) {
+            this.registeredCourses = registeredCourses;
+        }
 }
