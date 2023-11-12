604742dbd01818b494f18c7b289b4a3dc305bd0c
ardanlbnt <117982157+ardanlbnt@users.noreply.github.com>
2023-11-12 17:32:20 +0300

    Uploaded Lecturer class

---
+++ Iteration_1/RegistrationSystem/Lecturer.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Lecturer extends Staff {
+    
+    private String Proffesion;
+
+    public Lecturer(){
+
+    }
+    
+    public Lecturer(String Proffesion){
+        this.Proffesion=Proffesion;
+
+    }
+	public String getProffesion() {
+		return Proffesion;
+	}
+	public void setProffesion(String proffesion) {
+		Proffesion = proffesion;
+	}
+
+
+
+}
