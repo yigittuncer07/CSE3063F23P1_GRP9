390586928af32a97a7ca3756f38f1fb16f75555e
Umut Bayar <76680137+umutbayar@users.noreply.github.com>
2023-11-12 13:11:18 +0300

    Update Staff.java

--- Iteration_1/RegistrationSystem/Staff.java
+++ Iteration_1/RegistrationSystem/Staff.java
@@ -1,6 +1,30 @@
 import java.util.Scanner;
 
 public class Staff extends User{
-    public String staffID;
-    public String department;
+    private String staffID;
+    private String department;
+
+    public Staff() {
+    }
+
+    public Staff(String staffID, String department) {
+        this.staffID = staffID;
+        this.department = department;
+    }
+
+    public String getStaffID() {
+        return staffID;
+    }
+    
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+    
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
