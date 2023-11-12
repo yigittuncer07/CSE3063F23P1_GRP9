d6702b3075b9e68833f914a6f49c252fc7a6a12d
hasanozeren <92247872+hasanozeren@users.noreply.github.com>
2023-11-12 22:31:57 +0300

    Course added and course features

---
+++ Iteration_1/RegistrationSystem/Course.java
@@ -0,0 +1,94 @@
+package deneme;
+
+public class Course {
+	
+	
+	
+	  Student student = new Student();
+	  Advisor advisor = new Advisor();
+	  private  String courseName; 
+	  private String courseCode;
+	  Lecturer courseLecturer = new Lecturer();
+	  int credits;
+	  boolean prequisiteCompleted;
+	  Grade grade = new Grade();
+	  boolean completed;
+	  
+   	  public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public Advisor getAdvisor() {
+		return advisor;
+	}
+
+	public void setAdvisor(Advisor advisor) {
+		this.advisor = advisor;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
+	public Lecturer getCourseLecturer() {
+		return courseLecturer;
+	}
+
+	public void setCourseLecturer(Lecturer courseLecturer) {
+		this.courseLecturer = courseLecturer;
+	}
+
+	public int getCredits() {
+		return credits;
+	}
+
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+
+	public boolean isPrequisiteCompleted() {
+		return prequisiteCompleted;
+	}
+
+	public void setPrequisiteCompleted(boolean prequisiteCompleted) {
+		this.prequisiteCompleted = prequisiteCompleted;
+	}
+
+	public Grade getGrade() {
+		return grade;
+	}
+
+	public void setGrade(Grade grade) {
+		this.grade = grade;
+	}
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+
+	public Course() {
+		  
+	   }
+		
+
+}
