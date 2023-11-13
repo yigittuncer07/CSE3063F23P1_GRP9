cb324fdeb959205c9d3a34e65783f5477f3a5f4a
Mehmet Sina Çağlar <112509374+midnight-child@users.noreply.github.com>
2023-11-13 11:46:57 +0300

    Added Transcript.java class

---
+++ Iteration_1/RegistrationSystem/Transcript.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Transcript extends Course {
+    private ArrayList<Course> courses;
+    private double GPA;
+
+    public Transcript() {
+        this.courses = new ArrayList<>();
+        this.GPA = 0.0;
+    }
+
+    public void addCourse(Course course) {
+        courses.add(course);
+        updateGPA();
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    private void updateGPA() {
+        double totalCredits = 0.0;
+        double weightedSum = 0.0;
+
+        for (Course course : courses) {
+            totalCredits += course.getCredits();
+            weightedSum += course.getCredits() * convertHundredToGano(course.getOutOfHundred);
+        }
+
+        if (totalCredits > 0) {
+            GPA = weightedSum / totalCredits;
+        } else {
+            GPA = 0.0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("Transcript: \n");
+
+        for (Course course : courses) {
+            result.append("Course Code: ").append(course.getCourseCode())
+                    .append(", Course Name: ").append(course.getCourseName())
+                    .append(", Credit: ").append(course.getCredits())
+                    .append(", Letter Grade: ").append(course.grade.getLetterGrade())
+                    .append("\n");
+        }
+
+        result.append("GPA: ").append(GPA);
+        return result.toString();
+    }
+}
