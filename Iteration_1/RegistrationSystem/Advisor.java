87105102f19085159b0962ec160ed5f1949a688f
ardanlbnt <117982157+ardanlbnt@users.noreply.github.com>
2023-11-14 16:28:55 +0300

    Updated approval method in Advisor.java

--- Iteration_1/RegistrationSystem/Advisor.java
+++ Iteration_1/RegistrationSystem/Advisor.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 class Advisor extends Lecturer{
 
@@ -7,8 +9,22 @@ class Advisor extends Lecturer{
     public Advisor(){
 
     }
+	
     public void registrationApproval(ArrayList<Course> course){
-  
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Do you approve the draft? (Type 'yes' or 'no'):");
+        String userResponse = scanner.nextLine();
+
+        if (userResponse.equalsIgnoreCase("yes")) {
+            System.out.println("Draft approved!");
+        } else if (userResponse.equalsIgnoreCase("no")) {
+            System.out.println("Draft is not approved.");
+        } else {
+            System.out.println("Invalid input. Please type 'yes' or 'no'.");
+        }
+
+        scanner.close();
     }
 
 	public ArrayList<Course> getRegistrations() {
