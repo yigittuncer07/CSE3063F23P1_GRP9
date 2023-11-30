92aaae4b879f0fac083dc4800e52826d3101b024
Hasan Pekedis <hasanpekedis365@hotmail.com>
2023-11-30 16:15:31 +0300

    timer fixed

--- Iteration_1/RegistrationSystem/ExitTask.java
+++ Iteration_1/RegistrationSystem/ExitTask.java
@@ -3,7 +3,7 @@ import java.util.TimerTask;
 public class ExitTask extends TimerTask {
      @Override
         public void run() {
-            System.out.println("System will exit after 300 seconds.");
+            System.out.println("The system has timed out, please log in again.");
             System.exit(0);
         }
 }
