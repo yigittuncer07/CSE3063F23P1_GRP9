849ece21dec60449bf2c2dd74c1b60e0bca3fdb0
yigittuncer07 <yigittuncer@marun.edu.tr>
2023-12-12 20:15:18 +0300

    removed test print

--- Iteration_2/src/Advisor.java
+++ Iteration_2/src/Advisor.java
@@ -29,7 +29,6 @@ public class Advisor extends Lecturer {
         for (Draft draft : drafts) {
             for (Course course : draft.getCourses()) {
                 if (course.isApproved()) {
-                    System.out.println("\t\tSTUDENT NAME IN DRAFT: " + draft.getStudent().getName());
                     draft.getStudent().addToRegisteredCourses(course);
                 }
             }
