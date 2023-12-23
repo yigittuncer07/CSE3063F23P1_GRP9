aa0a53aac086497460202abfeb4ae7e2fe18b377
yigittuncer07 <yigittuncer@marun.edu.tr>
2023-12-23 22:29:40 +0300

    initial commit

--- Iteration_3/src/main.py
+++ Iteration_3/src/main.py
@@ -1,1 +1,25 @@
-print("hello world!")
+from classes.advisor import Advisor
+from classes.course import Course
+from classes.draft import Draft
+from classes.grade import Grade
+from classes.lecturer import Lecturer
+from classes.staff import Staff
+from classes.student_affairs_staff import Student_Affairs_Staff
+from classes.student import Student
+from classes.transcript import Transcript
+
+print("Hello, World!")
+
+advisor = Advisor()
+course = Course()
+draft = Draft()
+grade = Grade()
+lecturer = Lecturer()
+staff = Staff()
+student_affairs_staff = Student_Affairs_Staff()
+student = Student()
+transcript = Transcript()
+
+course.enroll_student(student)
+
+# Read all users into their arraylist
