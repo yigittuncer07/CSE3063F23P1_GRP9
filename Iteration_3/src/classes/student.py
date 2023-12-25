2b0fcfd966345e96039b77a54323e8786e1ab7e9
yigittuncer07 <yigittuncer@marun.edu.tr>
2023-12-25 18:12:36 +0300

    added get_eligable_courses method

--- Iteration_3/src/classes/student.py
+++ Iteration_3/src/classes/student.py
@@ -3,16 +3,45 @@ import json
 
 
 class Student(User):
-    def __init__(self, user_id, name, password, email):
+    def __init__(self, user_id=None, name=None, password=None, email=None, transcript=None, year=0):
         super().__init__(user_id,name,password,email)
+        self.transcript = transcript
+        self.year = year
 
     def get_name(self):
         return self.name
     
+    def get_transcript(self):
+        return self.transcript
+    
     def get_info(self):
         user_info = super().get_info()
         return f"user_id: {self.user_id}\nname: {self.name}\nemail: {self.email}"
+            
+    def is_course_eligible(self, course):
+        
+        # check year prerequisite
+        if course.get_course_year() < self.year:
+            return false
+        
+        # check if course is already taken 
+        if course.get_course_code() in self.transcript.get_completed_courses():
+            return false
+        
+        # check if prerequisites are completed 
+        for prerequisite in course.get_prerequisites():
+            if prerequisite not in self.transcript.get_completed_courses():
+                return false
 
+        return true
+    
+    def get_eligible_courses(self, all_courses):
+        eligible_courses = []
+        for course in all_courses:
+            if self.is_course_eligible(course):
+                eligible_courses.append(course)
+        return eligible_courses
+    
     def to_json_file(self):
         filename = f"database/students/{self.user_id}.json"
         with open(filename, 'w') as json_file:
