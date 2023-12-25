4bca5ead06a0692af083c3685f11024e05017575
yigittuncer07 <yigittuncer@marun.edu.tr>
2023-12-25 18:14:40 +0300

    added constructor and some getters

--- Iteration_3/src/classes/grade.py
+++ Iteration_3/src/classes/grade.py
@@ -1,2 +1,13 @@
 class Grade:
-    pass
+    
+    def __init__(self, course_code=None, number_grade=None, letter_grade=None, is_passed=None):
+        self.course_code = course_code
+        self.number_grade = number_grade
+        self.letter_grade = letter_grade
+        self.is_passed = is_passed
+        
+    def get_course_code(self):
+        return self.course_code
+
+    def is_grade_passing(self):
+        return self.is_passed
