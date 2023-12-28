c516fc39d30e5f8945338ce7111b39332bc48efe
yigittuncer07 <yigittuncer@marun.edu.tr>
2023-12-29 00:55:26 +0300

    added add to draft method and updated get_info method

--- Iteration_3/src/classes/advisor.py
+++ Iteration_3/src/classes/advisor.py
@@ -1,19 +1,50 @@
 from .lecturer import Lecturer
 import json
 
+
 class Advisor(Lecturer):
-    def __init__(self, user_id=None, name=None, password=None, email=None, department=None, field=None, students=None):
-        super().__init__(user_id,name,password,email,department,field)
+    def __init__(
+        self,
+        user_id=None,
+        name=None,
+        password=None,
+        email=None,
+        department=None,
+        field=None,
+        students=None,
+        drafts= [],
+    ):
+        super().__init__(user_id, name, password, email, department, field)
         self.students = students
+        self.drafts = drafts
+
+
+    # returns true if draft was appended
+    def add_draft(self, draft_sent):
+        for draft in self.drafts:
+            if draft_sent.get_student() == draft.get_student():
+                draft = draft_sent
+                return True
+        self.drafts.append(draft_sent)
+        return False  
+
+    @staticmethod
+    def get_list_info(list):
+        all_info = ""
+        for item in list:
+            all_info += item.get_info()
+        return all_info
 
     def get_info(self):
         user_info = super().get_info()
-        advisor_info = f"\nField: {self.field}\nStudents: {self.students}"
+        advisor_info = (
+            f"\nField: {self.field}\nStudents: {self.get_list_info(self.students)}\nDrafts: {self.get_list_info(self.students)}"
+        )
         return user_info + advisor_info
-        
+
     def to_json_file(self):
         filename = f"database/advisors/{self.user_id}.json"
-        with open(filename, 'w') as json_file:
+        with open(filename, "w") as json_file:
             json.dump(self.get_info(), json_file, indent=2)
         return filename
 
@@ -21,8 +52,8 @@ class Advisor(Lecturer):
     def from_json_file(cls, user_id):
         filename = f"{user_id}.json"
         if os.path.exists(filename):
-            with open(filename, 'r') as json_file:
+            with open(filename, "r") as json_file:
                 student_data = json.load(json_file)
                 return cls(**student_data)
         else:
-            raise FileNotFoundError(f"File {filename} does not exist.")     
+            raise FileNotFoundError(f"File {filename} does not exist.")
